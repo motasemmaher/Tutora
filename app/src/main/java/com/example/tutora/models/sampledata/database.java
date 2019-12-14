c18a16db42d8ca4c33e7354847960266c2384c98
ms126965 <mmsalem17@cit.just.edu.jo>
2019-12-14 19:28:07 +0200

    StartProject1

---
+++ app/src/main/java/com/example/tutora/models/sampledata/database.java
@@ -0,0 +1,30 @@
+package com.example.tutora.models.sampledata;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class database extends SQLiteOpenHelper{
+
+    public static final String DATABASE_NAME = "Tutor.db";
+    public database(Context context) {
+        super(context,DATABASE_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table Course(id integer primary key autoincrement,name text,time text,category text,price DECIMAL(7,3),image integer,rate integer)");
+      //  db.execSQL("create table CourseEnrolled (id integer primary key autoincrement, cousreId integer,completedLesson inegter, foreign key (courseId) references Course(id))");
+      //  db.execSQL("create table Login(username text primary key)");
+      //  db.execSQL("create table Student(username text primary key,email text,password text,image text,courseEnrolledId integer,courseWatchLaterId integer,lessonWatachLater integer," +
+               // "foreign key (courseEnrolledId) references CourseEnrolled(id)," +
+               // "foreign key (courseWatchLaterId) references Course(id)," +
+               // "foreign key (lessonWatachLater) references Lesson(id) )");
+        db.execSQL("create table Lesson (id integer primary key autoincrement,name text,link text,courseId integer, time text, reference text,task text,image integer, foreign key (courseId) references Course(id) )");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
