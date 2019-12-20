05084f0287fd4975020fcfb209bbe30ccceed8db
Osama <osalzoubi@gmail.com>
2019-12-20 16:13:11 +0200

    Added lesson fragment layout.

---
+++ app/src/main/java/com/example/tutora/ui/lesson/LessonFragment.java
@@ -0,0 +1,29 @@
+package com.example.tutora.ui.lesson;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tutora.R;
+
+public class LessonFragment extends Fragment {
+
+
+    public LessonFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lesson, container, false);
+    }
+
+}
