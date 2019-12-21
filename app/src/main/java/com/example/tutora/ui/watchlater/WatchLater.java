2b7a94d581dabd721f6746e1e762b8a2365ae691
ms126965 <mmsalem17@cit.just.edu.jo>
2019-12-21 23:19:22 +0200

    Revert "initiating project"
    This reverts commit d9ff9331ff9113d3fdd97df157ee8d7758e660f0.
    
    # Conflicts:
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/tutora/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/example/tutora/ui/watchlater/WatchLater.java
+++ app/src/main/java/com/example/tutora/ui/watchlater/WatchLater.java
@@ -3,6 +3,7 @@ package com.example.tutora.ui.watchlater;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tutora.CourseInfo;
 import com.example.tutora.EnrolledCourseInfo;
 import com.example.tutora.MainActivity;
 import com.example.tutora.R;
@@ -23,6 +25,7 @@ import com.example.tutora.models.sampledata.CourseEnrolled;
 import com.example.tutora.models.sampledata.Lesson;
 import com.example.tutora.models.sampledata.Login;
 import com.example.tutora.models.sampledata.Student;
+import com.example.tutora.ui.category.CourseAdapter;
 import com.example.tutora.ui.course.lessonadapter;
 import com.example.tutora.ui.mycourses.EnrolledCourseAdapter;
 
@@ -50,27 +53,51 @@ public class WatchLater extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         btnCourse = view.findViewById(R.id.CourseButton);
         btnLesson = view.findViewById(R.id.LessonButton);
-        RV = view.findViewById(R.id.listView_myCourses);
+        RV = view.findViewById(R.id.zxc);
         layoutManager = new LinearLayoutManager(view.getContext());
         RV.setLayoutManager(layoutManager);
+
+
         btnLesson.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Lesson lesson = new Lesson(view.getContext());
+                ArrayList<com.example.tutora.ui.course.Lesson> list2 = new ArrayList<>();
 
-                ArrayList<Lesson> list = new ArrayList<>();
-               // adapter = new lessonadapter(list);
+                Lesson lesson = new Lesson(view.getContext());
+                Cursor cursor = lesson.getLessons(2);
+                if(cursor.moveToFirst()){
+                    boolean flag = true;
+                    do{
+                        if(flag){
+                            flag = !flag;
+                            list2.add(new com.example.tutora.ui.course.Lesson(cursor.getString(1),cursor.getString(4),cursor.getInt(7),true));
+                        }else {
+                            list2.add(new com.example.tutora.ui.course.Lesson(cursor.getString(1),cursor.getString(4),cursor.getInt(7),false));
+                        }
+                    } while (cursor.moveToNext());
+                }
+
+                lessonadapter adapter= new lessonadapter(list2);
                 RV.setAdapter(adapter);
+               // adapter = new lessonadapter(list);
             }
 
         });
         btnCourse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ArrayList<CourseInfo> list = new ArrayList<>();
                 Course course = new Course(view.getContext());
-
-                ArrayList<EnrolledCourseInfo> list = new ArrayList<>();
-                adapter = new EnrolledCourseAdapter(list);
+                Cursor cur = course.getCategory("Programming");
+                if (cur.moveToFirst()) {
+                    do {
+                        Log.i("info", cur.getString(1) + "--" + cur.getString(2) + "--" + cur.getString(3) + "--"
+                                + cur.getString(4) + "--" + cur.getString(5) + "--" + cur.getString(6) + "--");
+                        CourseInfo ci = new CourseInfo(cur.getString(2), (float)cur.getDouble(5), cur.getInt(5), cur.getInt(6));
+                        list.add(ci);
+                    } while (cur.moveToNext());
+                }
+                adapter = new CourseAdapter(list);
                 RV.setAdapter(adapter);
             }
 
